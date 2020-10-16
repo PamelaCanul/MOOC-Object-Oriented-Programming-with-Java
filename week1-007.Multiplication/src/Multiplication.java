d291da29a6899a610472be88225a8a2883b26458
PamelaCanul <59714317+PamelaCanul@users.noreply.github.com>
2020-10-15 22:07:25 -0500

    Add files via upload

---
+++ week1-007.Multiplication/src/Multiplication.java
@@ -0,0 +1,14 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int a = 277;
+        int b = 111;
+
+        // Program your solution here. Remember to use variables a and b!
+        String toPrint= a+ "*" + b + "=" + (a*b); 
+        
+        System.out.println(toPrint);
+    }
+
+}
