21653e3423a80a02ddd7d090b5b9cc069c7c3b6f
PamelaCanul <59714317+PamelaCanul@users.noreply.github.com>
2020-10-15 22:04:33 -0500

    Add files via upload

---
+++ week1-006.Addition/src/Addition.java
@@ -0,0 +1,14 @@
+public class Addition {
+
+    public static void main(String[] args) {
+
+        int a = 73457;
+        int b =12888;
+        int result = a+b; // Fix this
+
+        String toPrint = a + " + " + b + " = " + result;
+        System.out.println(toPrint);
+
+    }
+
+}
