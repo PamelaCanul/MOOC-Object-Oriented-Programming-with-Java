8d12ff443674a92aaaf3a6450b43431fc7032c14
PamelaCanul <59714317+PamelaCanul@users.noreply.github.com>
2020-11-19 02:41:09 -0600

    Add files via upload

---
+++ week1-020.Usernames/src/Usernames.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Usernames {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+        System.out.println("Type your username: ");
+        String username = reader.nextLine(); 
+        
+        System.out.println("Type your password: ");
+        String password = reader.nextLine();
+        
+        if (username.equals("alex")){
+            if (password.equals("mightyducks")){
+                System.out.println("You are now logged into the system!");
+            } else{
+                System.out.println("Your username or password was invalid!");
+            }
+        } else if (username.equals("emily")){
+            if (password.equals("cat")){
+                System.out.println("You are now logged into the system!");
+            } else{
+                System.out.println("Your username or password was invalid!");
+            }
+        } else {
+            System.out.println("Your username or password was invalid!");
+        }
+        
+
+    }
+}
